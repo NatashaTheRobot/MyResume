1bdbd933735735774ccf528dadbd0e03a656f245
Natasha Murashev <nmurashev@gmail.com>
2013-11-16 08:26:18 -0800

    initial commit

---
+++ MyResume/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyResume
+//
+//  Created by Natasha Murashev on 11/16/13.
+//  Copyright (c) 2013 NatashaTheRobot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
